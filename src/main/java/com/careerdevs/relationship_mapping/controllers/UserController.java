3402719287cee19972b8f6c75bd433d77b6ca1f9
Travis J <tfauvelle@gmail.com>
2021-11-27 11:58:04 -0500

    user mappings.

--- src/main/java/com/careerdevs/relationship_mapping/controllers/UserController.java
+++ src/main/java/com/careerdevs/relationship_mapping/controllers/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -21,8 +22,35 @@ public class UserController {
         return repository.findAll();
     }
 
+    @GetMapping("/{id}")
+    public @ResponseBody User getById(@PathVariable long id) {
+        return repository.findById(id).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @GetMapping("/email/{email}")
+    public ResponseEntity<List<User>> findByEmail(@PathVariable String email) {
+        return new ResponseEntity<>(repository.findByEmail(email), HttpStatus.OK);
+    }
+
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User newUser) {
         return new ResponseEntity<>(repository.save(newUser), HttpStatus.CREATED);
     }
+
+    @PutMapping("/{id}")
+    public @ResponseBody User updateUserById(@PathVariable Long id, @RequestBody User updateData) {
+        User updatedUser = repository.findById(id).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        if(updateData.getName() != null) updatedUser.setName(updateData.getName());
+        if(updateData.getEmail() != null) updatedUser.setEmail(updateData.getEmail());
+
+        return repository.save(updatedUser);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> destroyUser(@PathVariable Long id) {
+        repository.deleteById(id);
+        return new ResponseEntity<>("Deleted", HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/careerdevs/relationship_mapping/repositories/UserRepository.java
+++ src/main/java/com/careerdevs/relationship_mapping/repositories/UserRepository.java
@@ -3,5 +3,9 @@ package com.careerdevs.relationship_mapping.repositories;
 import com.careerdevs.relationship_mapping.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    List<User>findByEmail(String email);
+
 }
