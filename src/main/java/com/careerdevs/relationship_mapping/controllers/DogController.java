52dd8b6237d59d2247ca6d2187b9b2505cdddfc6
Travis J <tfauvelle@gmail.com>
2021-11-27 20:22:15 -0500

    dog entity defined and mapped.

--- src/main/java/com/careerdevs/relationship_mapping/controllers/DogController.java
+++ src/main/java/com/careerdevs/relationship_mapping/controllers/DogController.java
@@ -1,11 +1,42 @@
 package com.careerdevs.relationship_mapping.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.careerdevs.relationship_mapping.models.Dog;
+import com.careerdevs.relationship_mapping.repositories.DogRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @CrossOrigin
 @RestController
-@RequestMapping("/dogs")
+@RequestMapping("/api/dogs")
 public class DogController {
+
+    @Autowired
+    private DogRepository repository;
+
+    @GetMapping
+    public @ResponseBody List<Dog> getAll() {
+        return repository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<Dog> createDog(@RequestBody Dog newDog) {
+        return new ResponseEntity<>(repository.save(newDog), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public @ResponseBody Dog updateDogById(@PathVariable Long id, @RequestBody Dog updateData) {
+        Dog updatedDog = repository.findById(id).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        if (updateData.getName() != null) updatedDog.setName(updateData.getName());
+        if (updateData.getAge() != null) updatedDog.setAge(updateData.getAge());
+        if ( updateData.getWeight() != null) updatedDog.setWeight(updateData.getWeight());
+
+        return repository.save(updatedDog);
+    }
+
 }
