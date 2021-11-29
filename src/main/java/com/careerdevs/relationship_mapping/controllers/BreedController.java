8c755d872bbea3f3cbf4974f1b54cc3361df4f72
Travis J <tfauvelle@gmail.com>
2021-11-29 08:02:26 -0500

    breed controller mapped.

--- src/main/java/com/careerdevs/relationship_mapping/controllers/BreedController.java
+++ src/main/java/com/careerdevs/relationship_mapping/controllers/BreedController.java
@@ -1,11 +1,15 @@
 package com.careerdevs.relationship_mapping.controllers;
 
 
+import com.careerdevs.relationship_mapping.models.Breed;
 import com.careerdevs.relationship_mapping.repositories.BreedRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @CrossOrigin
 @RestController
@@ -13,4 +17,30 @@ import org.springframework.web.bind.annotation.RestController;
 public class BreedController {
     @Autowired
     private BreedRepository repository;
+
+    @GetMapping
+    public @ResponseBody List<Breed> getAllBreeds() {
+        return repository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<Breed> createBreed(@RequestBody Breed newBreed) {
+        return new ResponseEntity<>(repository.save(newBreed), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public @ResponseBody Breed updateById(@PathVariable Long id, @RequestBody Breed updateData) {
+        Breed breed = repository.findById(id).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        if (updateData.getName() != null) breed.setName(updateData.getName());
+        if (updateData.getGroup() != null) breed.setGroup(updateData.getGroup());
+
+        return repository.save(breed);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> destroyBreed(@PathVariable Long id) {
+        repository.deleteById(id);
+        return new ResponseEntity<>("Deleted", HttpStatus.OK);
+    }
 }
