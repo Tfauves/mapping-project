33829768b8aa7f2b91fc03e00cea4408b256d511
Travis J <tfauvelle@gmail.com>
2021-12-02 19:31:57 -0500

    entry entity defined.

--- src/main/java/com/careerdevs/relationship_mapping/models/entry/Entry.java
+++ src/main/java/com/careerdevs/relationship_mapping/models/entry/Entry.java
@@ -1,15 +1,49 @@
 package com.careerdevs.relationship_mapping.models.entry;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import com.careerdevs.relationship_mapping.models.activity.Activity;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.List;
 
 @Entity
 public class Entry {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private Timestamp timeOfEntry;
 
+    @ManyToMany
+    private List<Activity> activityList;
 
+    public Entry() {}
+
+    public Entry(Timestamp timeOfEntry, List<Activity> activityList) {
+        this.timeOfEntry = timeOfEntry;
+        this.activityList = activityList;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Timestamp getTimeOfEntry() {
+        return timeOfEntry;
+    }
+
+    public void setTimeOfEntry(Timestamp timeOfEntry) {
+        this.timeOfEntry = timeOfEntry;
+    }
+
+    public List<Activity> getActivityList() {
+        return activityList;
+    }
+
+    public void setActivityList(List<Activity> activityList) {
+        this.activityList = activityList;
+    }
 }
